f8710db538e266120e71d7c3ee7a7a0ecb6d8532
raartist <rishabhzee98@gmail.com>
2021-01-06 17:24:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ChatBot from "react-chatbot-kit"
+import config from './chatbot/config';
+import MessageParser from './chatbot/MessageParser';
+import ActionProvider from './chatbot/ActionProvider';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className="chatbot" >
+     <ChatBot config={config} messageParser={MessageParser} actionProvider={ActionProvider} />
+     </div>
     </div>
   );
 }
---
+++ src/chatbot/config.js
@@ -0,0 +1,8 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+    botName: "Personal Bot",
+    initialMessages: [createChatBotMessage(`Hello, How could I help you?`)]
+  }
+  
+  export default config
